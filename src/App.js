3a685e3e9b663444bfb8895dc051adc64b4ebdcf
Tuyennq <tuyennguyencp99@gmail.com>
2020-10-07 15:35:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routers from './router/index';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <Routers />
+      </header>
+    </div>
+  );
+}
+
+export default App;
